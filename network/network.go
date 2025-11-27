68b9654cab387f9f702570ca7cd707cc421ea961
lljiang <lljiang@apac.freewheel.com>
2025-11-27 15:49:35 +0800

    final

--- network/network.go
+++ network/network.go
@@ -440,7 +440,7 @@ func MineTx(chain *blockchain.BlockChain) {
 	txs = append(txs, cbTx)
 
 	newBlock := chain.MineBlock(txs)
-	UTXOSet  := blockchain.UTXOSet{chain}
+	UTXOSet  := blockchain.UTXOSet{Blockchain:chain}
 	UTXOSet.Reindex()
 
 	fmt.Println("New Block mined")
